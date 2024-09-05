a5bba310bfb958ac20d6cfb8b1cb78e2a3df8fd4
ujwol <bktujwol@gmail.com>
2024-09-05 13:20:51 +0545

    Block display done

--- js/ctcl-frontend.js
+++ js/ctcl-frontend.js
@@ -220,7 +220,7 @@ class ctclMain {
     let loadingP = document.querySelector('.ctcl-product-loading');
     let discountTotal =  document.querySelector('input[name="total-discount"]');
     let totalDiscount = undefined != discountTotal ? parseFloat(discountTotal.value) : 0 ; 
-    Array.from(prodListCont.querySelectorAll('.ctcl-checkout-item,input,#ctcl-totalshipping-cost,#ctcl-subtotal-container,.ctcl-checkout-item-header,#ctcl-discount-cont,#ctcl-items-total-cont,#ctcl-tax-total-cont')).map(x => {
+    Array.from(prodListCont.querySelectorAll('.ctcl-checkout-item,input,#ctcl-checkout-list-container,#ctcl-totalshipping-cost,#ctcl-subtotal-container,.ctcl-checkout-item-header,#ctcl-discount-cont,#ctcl-items-total-cont,#ctcl-tax-total-cont')).map(x => {
         x.parentElement.removeChild(x)
     })
 
